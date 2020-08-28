9b30bbd7bd0ec5f058b4f3289bd628b515362cf8
derek <derek.guyagoes@gmail.com>
2020-08-28 15:53:02 -0600

    Initial commit from test area.

---
+++ main.py
@@ -0,0 +1,37 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-f", "--file", help="which compose file to parse")
+parser.add_argument("-d", "--destination", help="where will the images end up")
+
+args = parser.parse_args()
+
+images = []
+sourceImages = []
+
+f = open(args.file)
+for line in f:
+    if line.find("image") > -1:
+        colon = line.find(":") + 1
+        wholeline = str.strip(line[colon:])
+        forwardslash = wholeline.find("/")
+        if forwardslash > -1:
+            if wholeline[:forwardslash].find(args.destination) == -1:
+                image = wholeline[forwardslash + 1:]
+                images.append(image)
+            sourceImages.append(wholeline)
+
+f.close()
+
+f = open("send.sh", "w")
+f.write("#!/bin/sh\n\n")
+for image in images:
+    for sourceImage in sourceImages:
+        f.write('docker tag {0} {1}\n'.format(sourceImage, image))
+        f.write('docker push{0}/{1}\n\n'.format(args.destination, image))
+        break
+f.close()
+
+f = open("send.sh", "r")
+print(f.read())
